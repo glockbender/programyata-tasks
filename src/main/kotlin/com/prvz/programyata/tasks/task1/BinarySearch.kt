ec4293811416e925dfd5662691a22fed707ce4b0
Ilya Perevozchikov <Perevozchikov@arrival.com>
2020-08-29 15:37:34 +0300

    add kotlin version of task 1

--- src/main/kotlin/com/prvz/programyata/tasks/task1/BinarySearch.kt
+++ src/main/kotlin/com/prvz/programyata/tasks/task1/BinarySearch.kt
@@ -1,44 +1,109 @@
-// package com.prvz.programyata.tasks.task1
-//
-// class BinarySearch {
-//
-//    fun search(arr: IntArray, find: Int): Boolean {
-//
-//        // 1. Узнаем размер массива.
-//        // - если размер массива - четный, берем элемент справа
-//        // 2. Получаем индекс среднего элемента в массиве. Присваиваем переменной A
-//        // 3. Проверяем,
-//        // если A больше искомого, то берем левую часть массива. Возвращаемся к шагу 1
-//        // если A больше искомого, то берем правую часть массива. Возвращаемся к шагу 1
-//        // если A равно искомому, то выводим true
-//
-//        var innerArray: IntArray = arr
-//
-//        while (true) {
-//            b = b + 1
-//            println(b)
-//        }
-//
-//    }
-//
-//    fun searchRecursive(arr: IntArray, find: Int): Boolean {
-//
-//    }
-//
-//
-// }
-//
-// fun main() {
-//
-//
-//    BinarySearch().search(intArrayOf(1,2,3), 1)
-//
-//    // Получить массив чисел
-//
-//    // Получить число для поиска
-//
-//    // Вызвать функцию search
-//
-//    // Вывести результат выполнения функции
-//
-// }
+@file:Suppress("MagicNumber")
+package com.prvz.programyata.tasks.task1
+
+class BinarySearch {
+
+    /**
+     * Выполняет бинарный поиск.
+     * @param arr - ОТСОРТИРОВАННЫЙ челочисленный массив, в котором необходимо найти число
+     * @param find - Искомое число
+     * @return Индекс найденного элемента, или [-1] если элемент не найден
+     */
+    fun search(arr: IntArray, find: Int): Int {
+
+        var from = 0
+        var to = arr.size - 1
+
+        while (from <= to) {
+
+            val midIdx = (to + (to - from)) / 2
+            val midVal = arr[midIdx]
+
+            when {
+                midVal > find -> to = midIdx - 1
+                midVal < find -> from = midIdx + 1
+                else -> return midIdx
+            }
+        }
+
+        return -1
+    }
+
+    /**
+     * Рекурсивно выполняет бинарный поиск.
+     * @param arr - ОТСОРТИРОВАННЫЙ челочисленный массив, в котором необходимо найти число
+     * @param find - Искомое число
+     * @return true, если элемент найден. Иначе false
+     */
+    fun searchRecursive(arr: IntArray, find: Int): Boolean {
+
+        if (arr.size == 1) {
+            val value = arr[0]
+            return find == value
+        } else {
+            val midIdx = (arr.size - 1) / 2
+            val midVal = arr[midIdx]
+            val newArray = when {
+                midVal < find -> arr.copyOfRange(midIdx + 1, arr.size)
+                midVal > find -> arr.copyOfRange(0, midIdx)
+                else -> return true
+            }
+            return searchRecursive(newArray, find)
+        }
+    }
+}
+
+@Suppress("LongMethod", "ReturnCount")
+fun main() {
+
+    // Testing
+    BinarySearch().run {
+        val arr1 = intArrayOf(1, 2, 3)
+        val arr2 = intArrayOf(1, 2, 3, 4, 5, 6)
+        val arr3 = (0..1000000).toMutableList().toIntArray()
+
+        println(search(arr1, 1))
+        println(search(arr2, 1))
+        println(search(arr3, 100000))
+
+        println(searchRecursive(arr1, 1))
+        println(searchRecursive(arr2, 1))
+        println(searchRecursive(arr3, 100000))
+    }
+
+    println("Enter a sorted array to search and press \"Enter\" key.\nNote numbers must be divided by ','")
+    val arrIn = readLine()?.split(',')
+    if (arrIn == null) {
+        System.err.println("Input problems")
+        return
+    }
+    if (arrIn.isEmpty()) {
+        System.err.println("No data entered")
+        return
+    }
+    val arr = try {
+        arrIn.map(String::toInt).toIntArray()
+    } catch (nfe: NumberFormatException) {
+        System.err.println("Found illegal character")
+        return
+    }
+
+    println("Enter the desired number and press \"Enter\" key")
+    val findIn = readLine()
+    if (findIn == null) {
+        System.err.println("Input problems")
+        return
+    }
+    if (findIn.isBlank()) {
+        System.err.println("No data entered")
+        return
+    }
+    val find = try {
+        findIn.toInt()
+    } catch (nfe: NumberFormatException) {
+        System.err.println("Found illegal character")
+        return
+    }
+
+    println(BinarySearch().search(arr = arr, find = find))
+}
