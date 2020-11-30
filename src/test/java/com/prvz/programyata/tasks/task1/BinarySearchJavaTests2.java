3f5e0603cddd893fd27e4f9e434f1ac1fda0df6c
glockbender <Zahizu44>
2020-12-01 00:07:58 +0300

    add binary search test

---
+++ src/test/java/com/prvz/programyata/tasks/task1/BinarySearchJavaTests2.java
@@ -0,0 +1,40 @@
+package com.prvz.programyata.tasks.task1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BinarySearchJavaTests2 {
+
+    @Test
+    public void test() {
+
+        BinarySearchJava bsj = new BinarySearchJava();
+
+        int[] arr = new int[] { 1,3,5,7,9 };
+
+        int findTrue = 3;
+
+        int findFalse = 2;
+
+        boolean mustBeTrue = bsj.search(arr, findTrue);
+
+        boolean mustBeFalse = bsj.search(arr, findFalse);
+
+        Assertions.assertTrue(mustBeTrue);
+
+        Assertions.assertFalse(mustBeFalse);
+
+        findTrue = 9;
+
+        findFalse = 10;
+
+        mustBeTrue = bsj.search(arr, findTrue);
+
+        mustBeFalse = bsj.search(arr, findFalse);
+
+        Assertions.assertTrue(mustBeTrue);
+
+        Assertions.assertFalse(mustBeFalse);
+
+    }
+}
