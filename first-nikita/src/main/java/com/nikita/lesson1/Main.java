e8142f2ed52b4c4dd7bf5b06655bcf53a72257ce
nikol <nikolas141996@mail.ru>
2021-04-04 14:42:15 +0300

    lesson1 BubbleSort1

---
+++ first-nikita/src/main/java/com/nikita/lesson1/Main.java
@@ -0,0 +1,12 @@
+package com.nikita.lesson1;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 3, 6, 4, 8, 5};
+        new BubbleSort().sort(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+}
