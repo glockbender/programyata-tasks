04b29478b6fa68d18b2b5144752b17557ff0d818
nikol <nikolas141996@mail.ru>
2021-04-26 10:40:09 +0300

    Lesson4

---
+++ first-nikita/src/test/java/com/nikita/ArrayTestUtils.java
@@ -0,0 +1,35 @@
+package com.nikita;
+
+import java.util.Random;
+
+public class ArrayTestUtils {
+    // 10..20
+    // 20..10
+
+    public static int[] generate(int from, int to) {
+        int length = Math.abs(from - to) + 1;
+        int start = from;
+        int[] array = new int[length];
+        for (int i = 0; i < array.length; i++) {
+            if (from < to) {
+                array[i] = start++;
+            } else {
+                array[i] = start--;
+            }
+        }
+        return array;
+    }
+
+
+    // 10..20 , length 11
+    public static int[] generateRandom(int from, int to, int length) {
+        Random random = new Random();
+        int[] array = new int[length];
+        for (int i = 0; i < array.length; i++) {
+
+
+        }
+
+    }
+
+}
---
+++ first-nikita/src/test/java/com/nikita/lesson3_6/BinarySearchTest.java
@@ -0,0 +1,22 @@
+package com.nikita.lesson3_6;
+
+import com.nikita.lesson1.homework.binarysearch.BinarySearch;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BinarySearchTest {
+    @Test
+    public void BinarySearch_works_correctly() {
+        int[] array = new int[]{1, 3, 2, 4, 6, 5, 7};
+        int item=4;
+        new BinarySearch().binary(array, item, 1, 7);
+        Assertions.assertEquals(item,4);
+    }
+
+    @Test
+    public void BinarySearch_does_not_work_with_NULL() {
+        int[] array = null;
+        new BinarySearch().binary(array, 5, 1, 7);
+
+    }
+}
